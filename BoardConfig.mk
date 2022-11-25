4ec012419bffb043b264176f5ad4494c1243ab8f
NFS86 <jarbull86@gmail.com>
2022-11-25 13:16:40 +0000

    rosy: update prebuild kernel to NFS clang
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,6 +48,28 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/rosy
 TARGET_KERNEL_CONFIG := rosy-perf_defconfig
 TARGET_KERNEL_VERSION := 4.9
 TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := NFS
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-NFS
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
+TARGET_KERNEL_ADDITIONAL_FLAGS += \
+    LLVM=1 \
+    HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument" \
+    LLVM_IAS=1 \
+    CC=clang \
+    AR=llvm-ar \
+    AS=llvm-as \
+    LD=ld.lld \
+    NM=llvm-nm \
+    OBJCOPY=llvm-objcopy \
+    OBJDUMP=llvm-objdump \
+    OBJSIZE=llvm-size \
+    READELF=llvm-readelf \
+    STRIP=llvm-strip \
+    HOSTCC=clang \
+    HOSTCXX=clang++ \
+    HOSTLD=ld.lld \
+    HOSTAR=llvm-ar
 
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
